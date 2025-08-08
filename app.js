b14a1d8b300cf527dacb82fbfb5e5bce03130f9d
Daniel Nascimento <you@example.comgit config --global user.email you@example.com>
2025-08-08 03:23:22 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let listaGenerica = [];
+let linguagensDeProgramacao  = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+
+linguagensDeProgramacao.push('Java', 'Ruby','GoLang')
+
+console.log(linguagensDeProgramacao)
+
+let listaDeNomes = ['Daniel', 'Milone', 'Garrafa'];
+console.log(listaDeNomes[0])
+
+let listaDeFrutas = ['Banana', 'Maçã', 'Tomate'];
+console.log(listaDeFrutas[1])
+
+let listaDeCarros = ['Celta', 'Palio', 'Monza'];
+console.log(listaDeCarros[listaDeCarros.length -1]);
